f27f5670efea317b19de978b13dfad028b462e19
tao.zhang <qingfengmy@126.com>
2023-02-18 20:42:23 +0800

    no message

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
     // },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/qingfengmy/qingfengmy.github.io' }
     ],
 
     footer: {
